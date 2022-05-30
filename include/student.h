b1929dd8f2fecffcc54f2f430a44b38302a62b61
NinZeige <slow_d@icloud.com>
2022-05-30 22:53:49 +0800

    offer student methods

--- include/student.h
+++ include/student.h
@@ -4,13 +4,20 @@
 
 typedef struct 
 {
-    int id;         // 学号
-    char* name;     // 姓名
-    subject *sub;   // 成绩
-    int *is_valid;  // 学生的信息是否无误, 若有误则不参与计算
+    int id;             // 学号
+    char* name;         // 姓名
+    int *sub;       // 成绩
+    char **_input;       // input info
+    int _sub_num;        // number of sub
+    int _is_valid;      // 学生的信息是否无误, 若有误则不参与计算
 }student;
 
+student *new_student(int sub_num);
+void delete_student(student *stu);
+void try_converse(student *stu);
 int is_stu_valid(student *stu);
 int sort_student(student *stu, int num, int OPERATOR);
+
+int str_to_int(char *input, int length, int *result);
 
 #endif /* HIT_STU_H_ */
---
+++ src/student.c
@@ -0,0 +1,76 @@
+#include "student.h"
+#include <string.h>
+#include <stdlib.h>
+
+#define MAX_LENGTH 12
+
+
+void try_converse(student *stu)
+{
+    // read id
+    str_to_int(stu->_input[0], MAX_LENGTH, stu->id);
+    if (!stu->_is_valid) { return; }
+    strcpy(stu->name, stu->_input[1]);
+
+
+}
+
+/**
+ * @brief try parse str into int
+ * 
+ * @param input input string
+ * @param length the length of the str
+ * @param result the result int
+ * @return int is success
+ */
+int str_to_int(char *input, int length, int *result)
+{
+    *result = 0;
+    for (int i = 0; i < length; i++)
+    {
+        if (input[i])
+        {
+            if (input[i] >= '0' && input[i] <= '9')
+            {
+                *result = *result * 10 + input[i] - '0';
+            }
+            else
+            {
+                return 0;
+            }
+        }
+        else
+        {
+            return 1;
+        }
+    }
+    return 1;
+}
+
+student *new_student(int sub_num)
+{
+    student *res = (student *)malloc(sizeof(student));
+    res->id = 0;
+    res->name = (char *)malloc(sizeof(char) * MAX_LENGTH);
+    res->sub = (int *)malloc(sizeof(int) * sub_num);
+    res->_sub_num = sub_num;
+    res->_input = (char **)malloc(sizeof(char *) * (2 + sub_num));
+    for (int i = 0; i < sub_num + 2; i++)
+    {
+        res->_input[i] = (char *)malloc(sizeof(char) * MAX_LENGTH);
+    }
+    res->_is_valid = 0;
+
+    return res;
+}
+
+void delete_student(student *stu)
+{
+    free(stu->sub);
+    for (int i = 0; i < stu->_sub_num + 2; i++)
+    {
+        free(stu->_input);
+    }
+    free(stu);
+    return;
+}
